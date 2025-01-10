bf0758e6bd960882cfee1a2122e371657d9b9a9a
Priyanshu Rajan <priyanshumth0808@gmail.com>
2025-01-10 18:26:37 +0530

    add: Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("home");
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <img src={assets.logo} alt="Logo" className="logo" />
+      <ul className="navbar-menu">
+        <li onClick={()=>setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
+        <li onClick={()=>setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
+        <li onClick={()=>setMenu("order")} className={menu === "order" ? "active" : ""}>Order</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact Us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="search icon" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="basket" />
+        </div>
+        <div className="dot"></div>
+        <button>Sign In</button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
