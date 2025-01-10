fd0fe358438edda402c355a507ecdf61ba0c32a9
Priyanshu Rajan <priyanshumth0808@gmail.com>
2025-01-10 20:15:41 +0530

    add: explore menu section

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import "./Home.css";
 import Header from "../../components/Header/Header";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <ExploreMenu/>
     </div>
   );
 };
