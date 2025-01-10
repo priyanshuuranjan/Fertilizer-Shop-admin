881276093f16239dcf4970a5118fa70f304d8fa4
Priyanshu Rajan <priyanshumth0808@gmail.com>
2025-01-10 20:21:07 +0530

    add: active class and pass props to filter products

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
-      <ExploreMenu/>
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   );
 };
