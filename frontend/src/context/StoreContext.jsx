59c0289ddbdaf0682d52ce9e182c172a0e576317
Priyanshu Rajan <priyanshumth0808@gmail.com>
2025-01-11 15:47:17 +0530

    add: cart functionality i.e addToCart, removeFromCart, cartItems, setCartItems

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,10 +1,34 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { product_list } from "../assets/assets";
-export  const StoreContext = createContext(null)
+export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  // add to cart and remove to cart functionality
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
+  // pass context value to use in another components
   const contextValue = {
     product_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
   };
   return (
     <StoreContext.Provider value={contextValue}>
