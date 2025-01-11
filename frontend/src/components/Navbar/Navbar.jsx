31991fbc05766453877c854a14601816f49fa1b8
Priyanshu Rajan <priyanshumth0808@gmail.com>
2025-01-11 18:44:42 +0530

    add:  login section

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { assets } from "../../assets/assets";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
@@ -44,7 +44,14 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="basket" />
         </div>
         <div className="dot"></div>
-        <button>Sign In</button>
+
+        <button
+          onClick={() => {
+            setShowLogin(true);
+          }}
+        >
+          Sign In
+        </button>
       </div>
     </div>
   );
