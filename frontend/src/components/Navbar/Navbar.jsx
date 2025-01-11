785b7151a24f44861c51a590188c5174d2debbdf
Priyanshu Rajan <priyanshumth0808@gmail.com>
2025-01-11 18:16:01 +0530

    add: smooth scroll on active class

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -9,10 +9,34 @@ const Navbar = () => {
     <div className="navbar">
       <img src={assets.logo} alt="Logo" className="logo" />
       <ul className="navbar-menu">
-        <li onClick={()=>setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
-        <li onClick={()=>setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
-        <li onClick={()=>setMenu("order")} className={menu === "order" ? "active" : ""}>Order</li>
-        <li onClick={()=>setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact Us</li>
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          Menu
+        </a>
+        <a
+          href="#order"
+          onClick={() => setMenu("order")}
+          className={menu === "order" ? "active" : ""}
+        >
+          Order
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          Contact Us
+        </a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="search icon" />
