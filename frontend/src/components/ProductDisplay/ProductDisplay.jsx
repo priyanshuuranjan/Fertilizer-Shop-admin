b8b7364390f5358332dbd5e0738ba4ef9a5c76fc
Priyanshu Rajan <priyanshumth0808@gmail.com>
2025-01-11 16:23:34 +0530

    add: comment for better understanding to show selected item for menu

--- frontend/src/components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/components/ProductDisplay/ProductDisplay.jsx
@@ -11,6 +11,7 @@ const ProductDisplay = ({ category }) => {
       <h2 className="h2we">Top Fertilizers Near You</h2>
       <div className="product-display-list">
         {product_list.map((item, index) => {
+          // Display Product on clicking on each menu separately or if menu is not selected then it display all the products
           if (category === "All" || category === item.category) {
             return (
               <ProductItem
