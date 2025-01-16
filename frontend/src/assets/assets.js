9fe26ae644a18e08e570963c4b33708e7dcb90bb
Priyanshu Rajan <priyanshumth0808@gmail.com>
2025-01-16 18:43:46 +0530

    update: few improvement, nd testing

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -55,15 +55,15 @@ export const menu_list = [
     menu_image: menu_2,
   },
   {
-    menu_name: "Seeds",
+    menu_name: "Seed",
     menu_image: menu_2,
   },
   {
-    menu_name: "Pesticides",
+    menu_name: "Pesticide",
     menu_image: menu_2,
   },
   {
-    menu_name: "Herbicides",
+    menu_name: "Herbicide",
     menu_image: menu_4,
   },
   {
